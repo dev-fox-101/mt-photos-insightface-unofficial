4d8e0c5badf99b98e21ce80d1c325845ccbd4a90
dev-fox-101 <179208296+dev-fox-101@users.noreply.github.com>
2025-03-14 18:21:46 +0800

    Update server.py
    增加延迟加载模型和延迟重启释放内存

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ import cv2
 import asyncio
 from PIL import Image
 from io import BytesIO
+from fastapi.responses import HTMLResponse
 import insightface
 from insightface.utils import storage
 from insightface.app import FaceAnalysis
@@ -50,14 +51,21 @@ if on_win :
     current_folder = os.path.dirname(os.path.abspath(__file__))
     model_folder_path = os.path.join(current_folder, "_insightface_root")
 
-# 初始化人脸识别器
-faceAnalysis = FaceAnalysis(providers=['CUDAExecutionProvider', 'CPUExecutionProvider'],root=model_folder_path, allowed_modules=['detection', 'recognition'], name=recognition_model)
-faceAnalysis.prepare(ctx_id=0, det_thresh=detection_thresh, det_size=(640, 640))
+face_model = None
 
 
 async def check_inactive():
-    await asyncio.sleep(3600)
+    await asyncio.sleep(120) #2 min
     restart_program()
+
+
+def load_face_model():
+    global face_model
+    if face_model is None:
+        providers = ["CPUExecutionProvider"]
+        faceAnalysis = FaceAnalysis(providers=providers,root=model_folder_path, allowed_modules=['detection', 'recognition'], name=recognition_model)
+        faceAnalysis.prepare(ctx_id=0, det_thresh=detection_thresh, det_size=(640, 640))
+        face_model = faceAnalysis
 
 
 @app.middleware("http")
@@ -77,17 +85,35 @@ async def verify_header(api_key: str = Header(...)):
         raise HTTPException(status_code=401, detail="Invalid API key")
     return api_key
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 async def top_info():
-    return {"title": "unofficial face recognition api for mt-photos, get more info: https://github.com/kqstone/mt-photos-insightface-unofficial", "link": "https://mtmt.tech/docs/advanced/facial_api","detector_backend": detector_backend, "recognition_model": recognition_model}
-
-
+    html_content = """<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>MT Photos Insightface Server</title>
+    <style>p{text-align: center;}</style>
+</head>
+<body>
+<p style="font-weight: 600;">MT Photos人脸识别服务</p>
+<p>服务状态： 运行中</p>
+<p>使用方法： <a href="https://mtmt.tech/docs/advanced/insightface_api/">https://mtmt.tech/docs/advanced/insightface_api/</a></p>
+</body>
+</html>"""
+    return html_content
 
 
 @app.post("/check")
 async def check_req(api_key: str = Depends(verify_header)):
-    return {'result': 'pass'}
-
+    return {
+        'result': 'pass',
+        "title": "mt-photos-insightface",
+        "help": "https://mtmt.tech/docs/advanced/insightface_api",
+        "detector_backend": detector_backend,
+        "recognition_model": recognition_model,
+        "facial_min_score": detection_thresh, # 推荐的人脸最低置信度阈值
+        "facial_max_distance": 0.5, # 推荐的人脸差异值
+    }
 
 @app.post("/restart")
 async def check_req(api_key: str = Depends(verify_header)):
@@ -97,8 +123,8 @@ async def check_req(api_key: str = Depends(verify_header)):
 
 @app.post("/represent")
 async def process_image(file: UploadFile = File(...), api_key: str = Depends(verify_header)):
+    load_face_model()
     content_type = file.content_type
-
     image_bytes = await file.read()
     try:
         img = None
@@ -124,19 +150,8 @@ async def process_image(file: UploadFile = File(...), api_key: str = Depends(ver
             return {'result': [], 'msg': 'height or width out of range'}
 
         data = {"detector_backend": detector_backend, "recognition_model": recognition_model}
-        embedding_objs = await predict(_represent, img)
-        # embedding_objs = DeepFace.represent(
-        #     img_path=img,
-        #     detector_backend=detector_backend,
-        #     model_name=recognition_model,
-        #     enforce_detection=True,  # 强制检测，如果为true会报错, 设置为False时可以针对整张照片进行特征识别
-        #     align=True,
-        # )
-        #enforce_detection=True 时，未识别到人脸的错误信息
-        #1
-        # Face could not be detected in numpy array.Please confirm that the picture is a face photo or consider to set enforce_detection param to False.
-        #2
-        # Exception while extracting faces from numpy array.Consider to set enforce_detection arg to False.
+        embedding_objs = await predict(_represent, img, face_model)
+        # embedding_objs = _represent(img)  # DmlExecutionProvider使用异步并发时会导致程序退出
         del img
         data["result"] = embedding_objs
         # logging.info("detector_backend: %s", detector_backend)
@@ -154,27 +169,28 @@ async def process_image(file: UploadFile = File(...), api_key: str = Depends(ver
         print(e)
         return {'result': [], 'msg': str(e)}
 
-def _represent(img):
-  faces = faceAnalysis.get(img)
-  results = []
-  for face in faces:
-    resp_obj = {}
-    embedding = face.normed_embedding.astype(float)
-    resp_obj["embedding"] = embedding.tolist()
-    # print(len(resp_obj["embedding"]))
-    box = face.bbox
-    resp_obj["facial_area"] = {"x" : int(box[0]), "y" : int(box[1]), "w" : int(box[2] - box[0]), "h" : int(box[3] - box[1])}
-    resp_obj["face_confidence"] = face.det_score.astype(float) 
-    results.append(resp_obj)
-  return results
+def _represent(img, face_model):
+    faces = face_model.get(img)
+    results = []
+    for face in faces:
+        resp_obj = {}
+        embedding = face.normed_embedding.astype(float)
+        resp_obj["embedding"] = embedding.tolist()
+        # print(len(resp_obj["embedding"]))
+        box = face.bbox
+        resp_obj["facial_area"] = {"x" : int(box[0]), "y" : int(box[1]), "w" : int(box[2] - box[0]), "h" : int(box[3] - box[1])}
+        resp_obj["face_confidence"] = face.det_score.astype(float)
+        results.append(resp_obj)
+    return results
 
-async def predict(predict_func, img):
-    return await asyncio.get_running_loop().run_in_executor(None, predict_func, img)
+
+async def predict(predict_func, inputs,model):
+    return await asyncio.get_running_loop().run_in_executor(None, predict_func, inputs,model)
 
 def restart_program():
+    print("restart_program")
     python = sys.executable
     os.execl(python, python, *sys.argv)
 
-
 if __name__ == "__main__":
-    uvicorn.run("server:app", host="0.0.0.0", port=http_port)
+    uvicorn.run("server:app", host=None, port=http_port)
